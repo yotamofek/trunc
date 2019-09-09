cc46759c156fbe3b34c00d402375257896cf1a15
galoz <gal.oz@powtoon.com>
2019-09-09 11:37:00 +0300

    add more test scenarios - leading space and non-splitting emoji

--- src/lib.rs
+++ src/lib.rs
@@ -24,7 +24,15 @@ impl TruncateToBoundary for str {
             boundary = grapheme_boundary;
         }
 
-        &self[..boundary].trim_end()
+        for (grapheme_boundary, grapheme) in grapheme_iter {
+            if grapheme.chars().next().unwrap().is_whitespace() {
+                boundary = grapheme_boundary;
+            } else {
+                break;
+            }
+        }
+
+        &self[..boundary]
     }
 }
 
@@ -59,4 +67,39 @@ mod tests {
         assert_eq!(s.truncate_to_boundary(s.chars().count()), s);
         assert_eq!(s.truncate_to_boundary(0), "");
     }
+
+    #[test]
+    fn truncate_non_split_grapheme {
+        let s = "🤚🏾a🤚 🤚🏾\t 🤚    ";
+
+        assert_eq!(s.truncate_to_boundary(4), "🤚🏾a🤚");
+        assert_eq!(s.truncate_to_boundary(5), "🤚🏾a🤚");
+        assert_eq!(s.truncate_to_boundary(6), "🤚🏾a🤚");
+        assert_eq!(s.truncate_to_boundary(7), "🤚🏾a🤚 🤚🏾");
+        assert_eq!(s.truncate_to_boundary(8), "🤚🏾a🤚 🤚🏾");
+        assert_eq!(s.truncate_to_boundary(9), "🤚🏾a🤚 🤚🏾");
+        assert_eq!(s.truncate_to_boundary(10), "🤚🏾a🤚 🤚🏾\t 🤚");
+        assert_eq!(s.truncate_to_boundary(11), "🤚🏾a🤚 🤚🏾\t 🤚");
+        assert_eq!(s.truncate_to_boundary(12), "🤚🏾a🤚 🤚🏾\t 🤚");
+        assert_eq!(s.truncate_to_boundary(20), "🤚🏾a🤚 🤚🏾\t 🤚");
+
+    }
+
+    #[test]
+    fn truncate_non_split_grapheme_leading_whitespace() {
+        let s = "   🤚🏾a🤚 🤚🏾\t 🤚    ";
+
+        assert_eq!(s.truncate_to_boundary(4), "🤚🏾a🤚");
+        assert_eq!(s.truncate_to_boundary(5), "🤚🏾a🤚");
+        assert_eq!(s.truncate_to_boundary(6), "🤚🏾a🤚");
+        assert_eq!(s.truncate_to_boundary(7), "🤚🏾a🤚 🤚🏾");
+        assert_eq!(s.truncate_to_boundary(8), "🤚🏾a🤚 🤚🏾");
+        assert_eq!(s.truncate_to_boundary(9), "🤚🏾a🤚 🤚🏾");
+        assert_eq!(s.truncate_to_boundary(10), "🤚🏾a🤚 🤚🏾\t 🤚");
+        assert_eq!(s.truncate_to_boundary(11), "🤚🏾a🤚 🤚🏾\t 🤚");
+        assert_eq!(s.truncate_to_boundary(12), "🤚🏾a🤚 🤚🏾\t 🤚");
+        assert_eq!(s.truncate_to_boundary(20), "🤚🏾a🤚 🤚🏾\t 🤚");
+
+    }
 }
+
