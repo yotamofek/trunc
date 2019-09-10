e004cc8c000e6842a8911e7cc49c72e1fa835572
galoz <gal.oz@powtoon.com>
2019-09-10 16:11:30 +0300

    add truncate by byte offset and refactor truncate by bounds accordingly

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ use unicode_segmentation::UnicodeSegmentation;
 
 pub trait TruncateToBoundary {
     fn truncate_to_boundary(&self, chars: usize) -> &Self;
+    fn truncate_to_byte_offset(&self, count: usize) -> &Self;
 }
 
 impl TruncateToBoundary for str {
@@ -10,21 +11,27 @@ impl TruncateToBoundary for str {
             return &self[..0];
         }
 
-        let mut boundary = match self.trim_start().char_indices().nth(chars) {
-            None => return self,
-            Some((boundary, _)) => boundary,
+        let result = match self.char_indices().nth(chars) {
+            None => self,
+            Some((boundary, _)) => self.truncate_to_byte_offset(boundary)
         };
+        result
+    }
 
+    fn truncate_to_byte_offset(&self, boundary: usize) -> &Self {
+        if boundary > self.len() {
+            return &self
+        }
         let mut grapheme_iter = self
-            .grapheme_indices(true)
-            .rev()
-            .skip_while(move |(n, _)| *n > boundary);
-
+        .grapheme_indices(true)
+        .rev()
+        .skip_while(move |(n, _)| *n > boundary);
+        let mut bytecount = boundary;
         if let Some((grapheme_boundary, _)) = grapheme_iter.next() {
-            boundary = grapheme_boundary;
+            bytecount = grapheme_boundary;
         }
 
-        &self[..boundary].trim_end()
+        &self[..bytecount].trim_end()
     }
 }
 
@@ -48,7 +55,7 @@ mod tests {
         assert_eq!(s.truncate_to_boundary(9), "🤚🏾a🤚🏾 🤚🏾");
         assert_eq!(s.truncate_to_boundary(10), "🤚🏾a🤚🏾 🤚🏾");
         assert_eq!(s.truncate_to_boundary(11), "🤚🏾a🤚🏾 🤚🏾");
-        assert_eq!(s.truncate_to_boundary(12), "🤚🏾a🤚🏾 🤚🏾\t 🤚🏾");
+        assert_eq!(s.truncate_to_boundary(12), s);
     }
 
     #[test]
@@ -73,7 +80,7 @@ mod tests {
         assert_eq!(s.truncate_to_boundary(10), "🤚🏾a🤚 🤚🏾\t 🤚");
         assert_eq!(s.truncate_to_boundary(11), "🤚🏾a🤚 🤚🏾\t 🤚");
         assert_eq!(s.truncate_to_boundary(12), "🤚🏾a🤚 🤚🏾\t 🤚");
-        assert_eq!(s.truncate_to_boundary(20), "🤚🏾a🤚 🤚🏾\t 🤚    ");
+        assert_eq!(s.truncate_to_boundary(20), s);
     }
 
     #[test]
@@ -89,6 +96,21 @@ mod tests {
         assert_eq!(s.truncate_to_boundary(11), " 🤚🏾a🤚 🤚🏾\t 🤚");
         assert_eq!(s.truncate_to_boundary(12), " 🤚🏾a🤚 🤚🏾\t 🤚");
         assert_eq!(s.truncate_to_boundary(13), " 🤚🏾a🤚 🤚🏾\t 🤚");
-        assert_eq!(s.truncate_to_boundary(20), " 🤚🏾a🤚 🤚🏾\t 🤚    ");
+        assert_eq!(s.truncate_to_boundary(21), s);
+    }
+    #[test]
+    fn truncate_to_bytes(){
+        let s = "🤚🏾a🤚 ";
+        assert_eq!(s.truncate_to_byte_offset(0), "");
+        assert_eq!(s.truncate_to_byte_offset(1), "");
+        assert_eq!(s.truncate_to_byte_offset(2), "");
+        assert_eq!(s.truncate_to_byte_offset(7), "");
+        assert_eq!(s.truncate_to_byte_offset(8), "🤚🏾");
+        assert_eq!(s.truncate_to_byte_offset(9), "🤚🏾a");
+        assert_eq!(s.truncate_to_byte_offset(10), "🤚🏾a");
+        assert_eq!(s.truncate_to_byte_offset(13), "🤚🏾a🤚");
+        assert_eq!(s.truncate_to_byte_offset(14), "🤚🏾a🤚");
+        assert_eq!(s.truncate_to_byte_offset(18), s);
+        assert_eq!(s.truncate_to_byte_offset(100), s);
     }
 }
