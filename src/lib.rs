daa4185a34a094183a7e5bffbbc02a3a4b1aab10
galoz <gal.oz@powtoon.com>
2019-09-10 14:48:46 +0300

    remove start trim remove end trim when not outside of string bounds update tests accordingly

--- src/lib.rs
+++ src/lib.rs
@@ -11,12 +11,11 @@ impl TruncateToBoundary for str {
         }
 
         let mut boundary = match self.trim_start().char_indices().nth(chars) {
-            None => return self.trim(),
+            None => return self,
             Some((boundary, _)) => boundary,
         };
 
         let mut grapheme_iter = self
-            .trim_start()
             .grapheme_indices(true)
             .rev()
             .skip_while(move |(n, _)| *n > boundary);
@@ -25,7 +24,7 @@ impl TruncateToBoundary for str {
             boundary = grapheme_boundary;
         }
 
-        &self.trim_start()[..boundary].trim_end()
+        &self[..boundary].trim_end()
     }
 }
 
@@ -74,22 +73,22 @@ mod tests {
         assert_eq!(s.truncate_to_boundary(10), "🤚🏾a🤚 🤚🏾\t 🤚");
         assert_eq!(s.truncate_to_boundary(11), "🤚🏾a🤚 🤚🏾\t 🤚");
         assert_eq!(s.truncate_to_boundary(12), "🤚🏾a🤚 🤚🏾\t 🤚");
-        assert_eq!(s.truncate_to_boundary(20), "🤚🏾a🤚 🤚🏾\t 🤚");
+        assert_eq!(s.truncate_to_boundary(20), "🤚🏾a🤚 🤚🏾\t 🤚    ");
     }
 
     #[test]
-    fn truncate_non_split_grapheme_leading_whitespace() {
-        let s = "   🤚🏾a🤚 🤚🏾\t 🤚    ";
+    fn truncate_non_split_grapheme_with_whitespace() {
+        let s = " 🤚🏾a🤚 🤚🏾\t 🤚    ";
 
-        assert_eq!(s.truncate_to_boundary(4), "🤚🏾a🤚");
-        assert_eq!(s.truncate_to_boundary(5), "🤚🏾a🤚");
-        assert_eq!(s.truncate_to_boundary(6), "🤚🏾a🤚");
-        assert_eq!(s.truncate_to_boundary(7), "🤚🏾a🤚 🤚🏾");
-        assert_eq!(s.truncate_to_boundary(8), "🤚🏾a🤚 🤚🏾");
-        assert_eq!(s.truncate_to_boundary(9), "🤚🏾a🤚 🤚🏾");
-        assert_eq!(s.truncate_to_boundary(10), "🤚🏾a🤚 🤚🏾\t 🤚");
-        assert_eq!(s.truncate_to_boundary(11), "🤚🏾a🤚 🤚🏾\t 🤚");
-        assert_eq!(s.truncate_to_boundary(12), "🤚🏾a🤚 🤚🏾\t 🤚");
-        assert_eq!(s.truncate_to_boundary(20), "🤚🏾a🤚 🤚🏾\t 🤚");
+        assert_eq!(s.truncate_to_boundary(5), " 🤚🏾a🤚");
+        assert_eq!(s.truncate_to_boundary(6), " 🤚🏾a🤚");
+        assert_eq!(s.truncate_to_boundary(7), " 🤚🏾a🤚");
+        assert_eq!(s.truncate_to_boundary(8), " 🤚🏾a🤚 🤚🏾");
+        assert_eq!(s.truncate_to_boundary(9), " 🤚🏾a🤚 🤚🏾");
+        assert_eq!(s.truncate_to_boundary(10), " 🤚🏾a🤚 🤚🏾");
+        assert_eq!(s.truncate_to_boundary(11), " 🤚🏾a🤚 🤚🏾\t 🤚");
+        assert_eq!(s.truncate_to_boundary(12), " 🤚🏾a🤚 🤚🏾\t 🤚");
+        assert_eq!(s.truncate_to_boundary(13), " 🤚🏾a🤚 🤚🏾\t 🤚");
+        assert_eq!(s.truncate_to_boundary(20), " 🤚🏾a🤚 🤚🏾\t 🤚    ");
     }
 }
