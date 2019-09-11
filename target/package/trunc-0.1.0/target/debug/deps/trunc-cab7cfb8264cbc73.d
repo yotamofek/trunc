48ecdfd328de342e632cd9c7c8ab16ecd6723b20
galoz <gal.oz@powtoon.com>
2019-09-11 16:24:21 +0300

    remove target

--- target/package/trunc-0.1.0/target/debug/deps/trunc-cab7cfb8264cbc73.d
+++
@@ -1,7 +0,0 @@
-/Users/galoz/trunc/target/package/trunc-0.1.0/target/debug/deps/trunc-cab7cfb8264cbc73.rmeta: src/lib.rs
-
-/Users/galoz/trunc/target/package/trunc-0.1.0/target/debug/deps/libtrunc-cab7cfb8264cbc73.rlib: src/lib.rs
-
-/Users/galoz/trunc/target/package/trunc-0.1.0/target/debug/deps/trunc-cab7cfb8264cbc73.d: src/lib.rs
-
-src/lib.rs:
